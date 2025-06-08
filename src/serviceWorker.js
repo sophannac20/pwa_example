e8ac52680e9fe5557e57a2e9fa62b0452a0c2687
Sophanna <user@Sophanna.local>
2025-06-08 15:41:00 +0700

    init

---
+++ src/serviceWorker.js
@@ -0,0 +1,59 @@
+// src/serviceWorker.js
+
+const isLocalhost = Boolean(
+  window.location.hostname === 'localhost' ||
+    window.location.hostname === '[::1]' ||
+    window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
+);
+
+export function register(config) {
+  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+    const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
+    if (publicUrl.origin !== window.location.origin) {
+      return;
+    }
+
+    const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+
+    window.addEventListener('load', () => {
+      navigator.serviceWorker
+        .register(swUrl)
+        .then(registration => {
+          registration.onupdatefound = () => {
+            const installingWorker = registration.installing;
+            if (installingWorker == null) {
+              return;
+            }
+            installingWorker.onstatechange = () => {
+              if (installingWorker.state === 'installed') {
+                if (navigator.serviceWorker.controller) {
+                  if (config && config.onUpdate) {
+                    config.onUpdate(registration);
+                  }
+                } else {
+                  if (config && config.onSuccess) {
+                    config.onSuccess(registration);
+                  }
+                }
+              }
+            };
+          };
+        })
+        .catch(error => {
+          console.error('Error during service worker registration:', error);
+        });
+    });
+  }
+}
+
+export function unregister() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.ready
+      .then(registration => {
+        registration.unregister();
+      })
+      .catch(error => {
+        console.error('Error unregistering service worker:', error);
+      });
+  }
+}
